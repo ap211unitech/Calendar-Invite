d688fd0a952d3734a11ead6aa522bc8e9a728a41
AP <porwal.1@iitj.ac.in>
2022-05-17 17:27:21 +0530

    Zoom Meeting Link created

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const request = require("request");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var zoom_key = 'srBZmaYpR_-IrQ153krk6Q';
+var zoom_sec = '1H3hC1vzEeFkOJrPsbZBikeXLxjDroGMi2eO';
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
@@ -9,14 +26,13 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // time.
 const TOKEN_PATH = 'token.json';
 
-const sendInvitation = () => {
+const sendInvitation = (zoomResponse) => {
     // Load client secrets from a local file.
     fs.readFile('credentials.json', (err, content) => {
         if (err) return console.log('Error loading client secret file:', err);
         // Authorize a client with credentials, then call the Google Calendar API.
         // authorize(JSON.parse(content), listEvents);
-        authorize(JSON.parse(content), createEvent);
-
+        authorize(JSON.parse(content), zoomResponse, createEvent);
     });
 }
 
@@ -25,8 +41,9 @@ const sendInvitation = () => {
  * given callback function.
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
+ * @param {Object} zoomResponse Zoom response data
  */
-function authorize(credentials, callback) {
+function authorize(credentials, zoomResponse, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
@@ -35,7 +52,7 @@ function authorize(credentials, callback) {
     fs.readFile(TOKEN_PATH, (err, token) => {
         if (err) return getAccessToken(oAuth2Client, callback);
         oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+        callback(oAuth2Client, zoomResponse);
     });
 }
 
@@ -75,25 +92,77 @@ function getAccessToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 
-function createEvent(auth) {
+app.post('/schedule-meeting', (req, res) => {
+    // Creating Zoom Link
+    const config = {
+        email: "porwalarjun95@gmail.com" // API_KEY and API_SECRET are of this email. 
+    };
+
+    try {
+        const payload = {
+            iss: zoom_key,
+            exp: ((new Date()).getTime() + 5000)
+        };
+        const token = jwt.sign(payload, zoom_sec);
+
+        var options = {
+            method: "POST",
+            uri: `https://api.zoom.us/v2/users/${config.email}/meetings`,
+            body: {
+                topic: "Calendar Invitation",
+                type: 1,
+                settings: {
+                    host_video: "true",
+                    participant_video: "true"
+                }
+            },
+            auth: {
+                bearer: token
+            },
+            headers: {
+                "User-Agent": "Zoom-api-Jwt-Request",
+                "content-type": "application/json"
+            },
+            json: true //Parse the JSON string in the response
+        };
+
+        request(options, (error, response, body) => {
+            if (!error && response.statusCode === 201) {
+                // Triggering this function will allow to send Invitation
+                sendInvitation(response)
+                res.json({ msg: 'Success', response });
+
+            } else {
+                console.log(body);
+                res.send({ message: body.message });
+            }
+        });
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+})
+
+
+function createEvent(auth, zoomResponse) {
+
+
+    // return
+
     const calendar = google.calendar({ version: 'v3', auth });
     var event = {
         'summary': 'Calendar Invitation',
         'location': '3rd Floor, Ackruti Softech Park, M.I.D.C, Andheri E, Mumbai, Maharashtra 400093',
-        'description': 'Generates a google meet link and send it to attendes automatically',
+        'description': `<h3>Here is the link of zoom meeting. ${zoomResponse.body.join_url}</h3>`,
 
         // 30 Minutes duration (as of now)
         'start': {
-            'dateTime': new Date('Mon May 16 2022 7:30:00 GMT+0530 (India Standard Time)').toISOString(),
+            'dateTime': new Date('May 17 2022 13:22:00 GMT+0530 (India Standard Time)').toISOString(),
             'timeZone': 'Asia/Kolkata',
         },
         'end': {
-            'dateTime': new Date('Mon May 16 2022 8:00:00 GMT+0530 (India Standard Time)').toISOString(),
+            'dateTime': new Date('May 17 2022 13:45:00 GMT+0530 (India Standard Time)').toISOString(),
             'timeZone': 'Asia/Kolkata',
         },
-        // 'recurrence': [
-        //     'RRULE:FREQ=DAILY;COUNT=2'
-        // ],
         'attendees': [
             { 'email': 'porwal.1@iitj.ac.in' },
             { 'email': 'porwalarjun95@gmail.com' },
@@ -105,12 +174,6 @@ function createEvent(auth) {
                 { 'method': 'popup', 'minutes': 10 },   // 10 minutes from mobile notification
             ],
         },
-        conferenceData: {
-            createRequest: {
-                requestId: "random_request_ID",
-                conferenceSolutionKey: { type: "hangoutsMeet" },
-            },
-        },
     };
 
     calendar.events.insert({
@@ -118,16 +181,18 @@ function createEvent(auth) {
         calendarId: 'primary',
         sendUpdates: 'all',
         sendNotifications: true,
-        conferenceDataVersion: 1,
         resource: event,
     }, function (err, event) {
         if (err) {
             console.log('There was an error contacting the Calendar service: ' + err);
             return;
         }
-        console.log('Event created: %s', event.data.htmlLink);
+        console.log('Event created');
     });
 }
 
-// Triggering this function will allow to send Invitation
-sendInvitation()
+// Run Server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
